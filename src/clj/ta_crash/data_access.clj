00fb8b9d547522272eba5a860582c01cc60dbdb8
Akil Harris <akil.harris@gmail.com>
2015-07-02 22:31:32 -0400

    working on total display

--- src/clj/ta_crash/data_access.clj
+++ src/clj/ta_crash/data_access.clj
@@ -2,55 +2,26 @@
   (:require
     [clojure.string :refer [split]]))
 
-
-;; a data point
-; {:type "precinct"
-;  :identifier "83"
-;  :year 2015
-;  :month 0
-;  :total-crashes 103
-;  :total-with-injured 76
-;  :total-with-death 4
-;  :people-injured 98
-;  :bicyclists-injured 10
-;  :pedestrians-injured 58
-;  :motorists-injured 30
-;  :people-killed 5
-;  :bicyclists-killed 1
-;  :pedestrians-killed 3
-;  :motorists-killed 1
-;  :contributing-factors [[:lost-consciousness 10]
-;                         [:oversized-vehicle 4]
-;                         [:driver-inexperience 21]
-;                         [:prescription-medication 11]
-;                         [:unspecified 83]
-;                         [:fatigued-drowsy 32]
-;                         [:backing-unsafely 32]
-;                         [:driver-inattention 64]]
-;  :vehicle-types [[:passenger-vehicle 93]
-;                  [:unknown 83]
-;                  [:van 30]
-;                  [:truck 19]
-;                  [:bicycle 10]
-;                  [:sport-utility 43]]}
-
 (defn build-point
   [year month]
   {:type "precinct"
    :identifier "83"
    :year year
    :month month
-   :total-crashes (rand-int 200)
-   :total-with-injured (rand-int 101)
-   :total-with-death (rand-int 13)
-   :people-injured (rand-int 101)
-   :bicyclists-injured (rand-int 25)
-   :pedestrians-injured (rand-int 25)
-   :motorists-injured (rand-int 25)
-   :people-killed (rand-int 13)
-   :bicyclists-killed (rand-int 8)
-   :pedestrians-killed (rand-int 8)
-   :motorists-killed (rand-int 8)
+   :totals [{:type :crashes
+             :total-crashes (rand-int 200)
+             :total-with-injured (rand-int 101)
+             :total-with-death (rand-int 13)]
+            {:type :injured
+             :people-injured (rand-int 101)
+             :bicyclists-injured (rand-int 25)
+             :pedestrians-injured (rand-int 25)
+             :motorists-injured (rand-int 25)}
+            {:type :killed
+             :people-killed (rand-int 13)
+             :bicyclists-killed (rand-int 8)
+             :pedestrians-killed (rand-int 8)
+             :motorists-killed (rand-int 8)}]
    :contributing-factors [[:lost-consciousness (rand-int 95)]
                           [:oversized-vehicle (rand-int 95)]
                           [:driver-inexperience (rand-int 95)]
--- src/cljs/ta_crash/totals.cljs
+++ src/cljs/ta_crash/totals.cljs
@@ -2,13 +2,34 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
+(defn get-title-display
+  [{:keys [type]} :as total]
+  (cond
+    (= type :crashes) (str "Total Crashes " (:total-crashes total))
+    (= type :injured) (str "Total Injuries " (:people-injured total))
+    (= type :killed) (str "Total Deaths " (:people-killed total))))
+
+(get-type-display
+  [type]
+  (cond
+    (= type :total-with-injured) (str "Injury")
+    (= type :total-with-death) (str "Death")
+    (or (= type :pedestrians-injured) (= type :pedestrians-killed)) (dom/i #js {:className "icon-pedestrians"})
+    (or (= type :bicyclists-injured) (= type :bicyclists-killed)) (dom/i #js {:className "icon-bicyclists"})
+    (or (= type :motorists-injured) (= type :motorists-killed)) (dom/i #js {:className "icon-motorists"})))
+
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~
+; Total Display Components
+; ~~~~~~~~~~~~~~~~~~~~~~~~~
+
 (defn total-item-display
   [[type val]]
   (reify
     om/IRender
     (render [this]
       (dom/div #js {:className "total-item"}
-        (dom/p nil val)
+        (dom/p #js {:className "total-item-count"} val)
         (dom/p nil (get-type-display type))))))
 
 (defn total-group-item
@@ -25,7 +46,11 @@
     om/IRender
     (render [this]
       (println "total-group: " total)
-      (om/build total-display total))))
+      (om/dom section nil
+        (om/dom p #js {:className "total-title"}
+          (get-title-display total))
+        (apply dom/ul #js {:className "total-group-items"}
+          (om/build-all total-group-item (total)))))))
 
 (defn total-groups-view
   [data owner]
